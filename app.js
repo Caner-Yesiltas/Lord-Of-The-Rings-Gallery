0aaf6acc2c55ce3ea6dcbe689a5fb9c0584a5b25
CanerYesiltas <caneryesiltas1@gmail.com>
2024-08-11 18:49:21 -0400

    fetch function start to build up

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ const loadingDiv = document.getElementById("loading");
 const btn = document.querySelector(".btn");
 const containerDiv = document.querySelector(".container");
 const tarih = document.getElementById("tarih");
+
+
+
+setTimeout(() => {
+
+    loadingDiv.style.display="none";
+    containerDiv.style.display="flex";
+
+
+    
+}, 3000);
+
+function fetchImages() {
+    fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+      .then(response => response.json())
+      .then(data => {
+        
+        console.log(data);
+      });
+  }
+  
+  fetchImages();
