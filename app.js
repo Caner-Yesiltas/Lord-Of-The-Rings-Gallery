03074996af2b6896a6e46296bc51856b66b6ef2f
CanerYesiltas <caneryesiltas1@gmail.com>
2024-08-12 03:06:28 -0400

    lord of the rings added

--- app.js
+++ app.js
@@ -3,71 +3,85 @@ const loadingDiv = document.getElementById("loading");
 const btn = document.querySelector(".btn");
 const containerDiv = document.querySelector(".container");
 const tarih = document.getElementById("tarih");
+const loading2 =document.getElementById("loading")
 
 containerDiv.style.display = "none";
 
+
+function updateTarih() {
+    const now = new Date();
+    const formattedDate = now.toLocaleString(); 
+    tarih.innerText = `Son güncelleme: ${formattedDate}`; 
+  }
+  
+  
+  updateTarih();
+ 
 setTimeout(() => {
+  loadingDiv.style.display = "none";
+  containerDiv.style.display = "flex";
+  loading2.style.opacity = "0.1";
+ 
+  getLotrImages();
+}, 2000);
+loading2.style.opacity = "0.1";
+ 
+function getLotrImages() {
+    loading2.style.opacity = "0.5";
+  cardDiv.innerHTML = `<img src="./img/loading.gif"/>`;
+  
+  const flickrApiKey = "6f7487affd74f71e1148975206f1d0a0";
+  const randomPage = Math.floor(Math.random() * 100) + 1;
+  const flickrApiUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrApiKey}&tags=lord+of+the+rings&per_page=5&page=${randomPage}&format=json&nojsoncallback=1`;
 
-    loadingDiv.style.display="none";
-    containerDiv.style.display="flex";
-    getLotrImages();
-
-    
-}, 3000);
-
-function getLotrImages () {
-    cardDiv.innerHTML = `<img src="./img/loading.gif"/>`;
-    const flickrApiKey = "J0464b5b099ff8d2f8885d3077787c70d";
-    const flickrApiSecret = "c8108ff7e7352dc9";
-    const flickrApiUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrApiKey}&tags=lord+of+the+rings&per_page=10&format=json&nojsoncallback=1`;
-
-    fetch(flickrApiUrl)
-  .then((res) => {
-    if(!res.ok) {
+  fetch(flickrApiUrl)
+    .then((res) => {
+      if (!res.ok) {
         throw new Error("Veri Cekme Islemi Basarisiz");
 
         // verinin dgoru gelip gelmedigi kontrol edilir
-        
-    }
-    
-    return res.json();
-  })
-  
-  .then((data) => { displayCardImages(data.photos.photo); // photos.photo datanin yolu then api ile cektigimiz 
+      }
+      loading2.style.opacity = "0.1";
+ 
+      return res.json();
+    })
 
-    //buraya istedigimiz islem yazilir verinin alindigi kisim
-    
-  })
+    .then((data) => {
+      console.log(data);
 
+      displayCardImages(data.photos.photo); // photos.photo datanin yolu then api ile cektigimiz
 
-  .catch((error) => {
+      //buraya istedigimiz islem yazilir verinin alindigi kisim
+      updateTarih();
+    })
 
-    // burayada hata mesaji
-    console.error("Hata:", error);
-    cardDiv.innerHTML = '<img src="./img/error.gif" />';
-    alert("Verileri çekerken bir hata oluştu. Lütfen daha sonra tekrar deneyin.");
-     
-  });
+    .catch((error) => {
+      // burayada hata mesaji
+      console.error("Hata:", error);
+      cardDiv.innerHTML = '<img src="./img/error.gif" />';
+      alert(
+        "Verileri çekerken bir hata oluştu. Lütfen daha sonra tekrar deneyin."
+      );
+    });
 }
 
+getLotrImages();
 
-getLotrImages()
-
-  btn.onclick = () => getLotrImages();
-
-
-    
+btn.onclick = () => {
   
-  function displayCardImages(data) {
-    cardDiv.innerHTML="";
+    
+    getLotrImages();
+};
 
-    data.forEach((cat) => { // cat datanin icerisindeki her bir verinin adi parametre olarak geceriz 
+function displayCardImages(photos) {
+  cardDiv.innerHTML = "";
+
+  photos.forEach(({ farm, server, id, secret, title }) => {
+    // cat datanin icerisindeki her bir verinin adi parametre olarak geceriz simdi onu photo olarak yaptik api degisti
 
     const imgElement = document.createElement("img");
-    imgElement.src =cat.url;
+    imgElement.src = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_b.jpg`; //   imgElement.src =cat.url;  onceki deger buydu degistirildi
     imgElement.classList.add("img-fluid", "col-md-4", "col-sm-12"); // classList.add ile bootstrap classlari ekledik olusturdugumuz img elementine
-cardDiv.appendChild(imgElement);
-
-    });
-
-  };
+    cardDiv.appendChild(imgElement);
+  });
+}
