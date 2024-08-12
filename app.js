45b55cb739ddae3add7ddbb352e112c6c8395b8a
CanerYesiltas <caneryesiltas1@gmail.com>
2024-08-11 22:39:12 -0400

    displaycardimages function start to build

--- app.js
+++ app.js
@@ -16,12 +16,58 @@ setTimeout(() => {
 }, 3000);
 
 function fetchImages() {
+    cardDiv.innerHTML = `<img src="./img/loading.gif"/>`;
     fetch("https://api.thecatapi.com/v1/images/search?limit=10")
-      .then(response => response.json())
-      .then(data => {
+
+  .then((res) => {
+    if(!res.ok) {
+        throw new Error("Veri Cekme Islemi Basarisiz");
+
+        // verinin dgoru gelip gelmedigi kontrol edilir
         
-        console.log(data);
-      });
-  }
+    }
+    
+    return res.json();
+  })
   
+  .then((data) => { displayCardImages();
+
+    //buraya istedigimiz islem yazilir verinin alindigi kisim
+    
+  })
+
+
+  .catch((error) => {
+
+    // burayada hata mesaji
+    console.error("Hata:", error);
+    cardDiv.innerHTML = '<img src="./img/error.gif" />';
+    alert("Verileri çekerken bir hata oluştu. Lütfen daha sonra tekrar deneyin.");
+     
+  });
+}
+
+
   fetchImages();
+
+  btn.addEventListener("click", fetchImages) {
+
+
+
+  }
+
+
+    
+  
+  function displayCardImages(data) {
+    cardDiv.innerHTML="";
+
+    data.forEach((cat) => { // cat datanin icerisindeki her bir verinin adi parametre olarak geceriz 
+
+    const imgElement = document.createElement("img");
+
+    
+
+    })
+
+  }
